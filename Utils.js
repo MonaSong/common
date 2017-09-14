e0597bbb10cfdd432c0bde4cd6d40e6fd8e0d358
Qingqing Song <qqsong@niub.la>
2017-09-14 19:26:40 +0800

    增加core js

---
+++ Utils.js
@@ -0,0 +1,80 @@
+var Utils = {
+  /**
+   * [storage description] 本地存储
+   * @type {Object}
+   */
+  storage:{
+    setSessionStore: function (key, val) {
+      window.sessionStorage[key] = val
+    },
+    getSessionStore: function (key) {
+      return window.sessionStorage[key]
+    },
+    clearSessionStore: function () {
+      window.sessionStorage.clear()
+    },
+    setLocalStore: function (key, val) {
+      window.sessionStorage[key] = val
+    },
+    getLocalStore: function (key) {
+      return window.sessionStorage[key]
+    },
+    clearLocalStorage: function () {
+      window.localStorage.clear()
+    }
+  },
+  /**
+   * @description  获取地址栏中的参数
+   * @return {Object} 返回地址栏中的查询参数对象
+   */
+  getHrefData: function () {
+   var idx0 = ref.indexOf('?')
+   var data = {}
+   if (idx0 > -1) {
+     var c_data = ref.substring(idx0 + 1)
+     var idx1 = c_data.indexOf('&')
+     if (idx1 > -1) {
+        c_data = c_data.split('&')
+        $.each(c_data, function (i, item) {
+          var cur = item.split('=')
+          data[cur[0]] = cur[1]
+      })
+     } else {
+      var cur = c_data.split('=')
+      data[cur[0]] = cur[1]
+     }
+   } 
+   return data
+  },
+  getCookie: function (name) {
+      var match = document.cookie.match(new RegExp(name + '=([^;]+)'));
+      if (match) return match[1];
+  },
+  setCookie: function (name, value) {
+      var exp = new Date();
+      exp.setTime(exp.getTime() + 365 * 24 * 3600 * 1000);
+      document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+  },
+  //获取当前协议
+  getProtocol: function () {
+    return window.location.protocol
+  },
+  //获取当前域名信息
+  getUrlDomain: function () {
+      return window.location.protocol + "//" + window.location.host;
+  },
+  getHashParams: function () {
+      var hash = location.hash;
+      var tmpArr = hash.split("&");
+      var parmObj = {};
+      if (tmpArr.length > 0 && tmpArr[0] !== "") {
+          for (var i = 0, len = tmpArr.length; i < len; i++) {
+              var tmp = tmpArr[i].replace("#", "").split("=");
+              parmObj[tmp[0]] = tmp[1];
+          }
+      } else {
+          parmObj = {};
+      }
+      return parmObj;
+  }
+}
