1681cb75111aed787523ab3ec4f1564ae6d1e09f
Qingqing Song <qqsong@niub.la>
2017-11-03 15:25:41 +0800

    fx

--- Utils.js
+++ Utils.js
@@ -76,5 +76,24 @@ var Utils = {
           parmObj = {};
       }
       return parmObj;
+  },
+  hasClass: function (elem, cls) {
+    cls = cls || '';
+    if (cls.replace(/\s/g, '').length == 0) return false; //当cls没有参数时，返回false
+    return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
+  },
+  addClass: function(ele, cls) {
+    if (!hasClass(elem, cls)) {
+      ele.className = ele.className == '' ? cls : ele.className + ' ' + cls;
+    }
+  },
+  removeClass: function (ele, cls) {
+    if (hasClass(elem, cls)) {
+      var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, '') + ' ';
+      while (newClass.indexOf(' ' + cls + ' ') >= 0) {
+        newClass = newClass.replace(' ' + cls + ' ', ' ');
+      }
+      elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
   }
 }
